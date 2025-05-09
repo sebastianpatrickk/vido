322ce784cc957fcf5f6ebe997c789915e8da4078
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-05-09 11:21:11 +0200

    Add google oauth envs and google provider

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,17 @@ export const env = createEnv({
   server: {
     KV_REST_API_URL: z.string().url(),
     KV_REST_API_TOKEN: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
   },
-  // client: {},
+  client: {
+    NEXT_PUBLIC_SITE_URL: z.string().url(),
+  },
   runtimeEnv: {
     KV_REST_API_URL: process.env.KV_REST_API_URL,
     KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
   },
 });
