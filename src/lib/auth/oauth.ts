cf6bec84ee2d88e8a43eff808a4769b76c162804
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-05-22 10:37:43 +0200

    remove old project for easy convex setup

--- src/lib/auth/oauth.ts
+++
@@ -1,8 +0,0 @@
-import { Google } from "arctic";
-import { env } from "@/env";
-
-export const google = new Google(
-  env.GOOGLE_CLIENT_ID,
-  env.GOOGLE_CLIENT_SECRET,
-  `${env.NEXT_PUBLIC_SITE_URL}/api/auth/google/callback`,
-);
