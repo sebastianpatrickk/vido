b8b55db745c249e669085efbbd474ab81ef90035
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-07-08 13:43:17 +0200

    Update constants to include Cloudflare R2 configuration placeholders and clear Convex HTTP URL

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -1,2 +1,8 @@
 export const SUPPORTED_FORMATS = [".mp4", ".mov", ".avi", ".mkv"]
-export const CONVEX_HTTP_URL = "https://glorious-snake-655.convex.site"
+export const CONVEX_HTTP_URL = ""
+
+export const CLOUDFLARE_R2_ACCOUNT_ID = ""
+export const CLOUDFLARE_R2_BUCKET_NAME = ""
+export const CLOUDFLARE_R2_REGION = ""
+export const CLOUDFLARE_R2_ACCESS_KEY_ID = ""
+export const CLOUDFLARE_R2_SECRET_ACCESS_KEY = ""
